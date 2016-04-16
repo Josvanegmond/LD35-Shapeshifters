3c5f40eee48cd2cfaed02a6c46f8387006102e5b
josvanegmond <jostievanegmond@gmail.com>
2016-04-16 10:49:09 +0200

    First setup

---
+++ desktop/src/nl/joozey/shapeshifter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nl.joozey.shapeshifter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nl.joozey.shapeshifter.ShapeshifterMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShapeshifterMain(), config);
+	}
+}
